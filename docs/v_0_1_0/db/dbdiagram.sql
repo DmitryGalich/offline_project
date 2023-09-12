e3e0c5865bb6be92d04058b4181facf605080b5b
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-09-12 23:24:33 +0300

    123

--- docs/v_0_1_0/db/dbdiagram.sql
+++ docs/v_0_1_0/db/dbdiagram.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "user" (
   "id" uuid PRIMARY KEY,
-  "login" varchar,
-  "password" varchar,
-  "mail" varchar,
-  "phone" varchar,
-  "first_name" varchar,
-  "second_name" varchar,
-  "date_of_birth" date,
-  "created_at" timestamp
+  "login" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "mail" varchar NOT NULL,
+  "phone" varchar NOT NULL,
+  "first_name" varchar NOT NULL,
+  "second_name" varchar NOT NULL,
+  "date_of_birth" date NOT NULL,
+  "created_at" timestamp NOT NULL
 );
 
 CREATE TABLE "chat" (
@@ -16,10 +16,10 @@ CREATE TABLE "chat" (
 
 CREATE TABLE "chat_comment" (
   "id" uuid PRIMARY KEY,
-  "chat" uuid,
-  "user" uuid
+  "chat" uuid NOT NULL,
+  "author" uuid NOT NULL
 );
 
 ALTER TABLE "chat_comment" ADD FOREIGN KEY ("chat") REFERENCES "chat" ("id");
 
-ALTER TABLE "chat_comment" ADD FOREIGN KEY ("user") REFERENCES "user" ("id");
+ALTER TABLE "chat_comment" ADD FOREIGN KEY ("author") REFERENCES "user" ("id");
