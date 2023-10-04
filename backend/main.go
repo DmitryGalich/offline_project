e41d9627189ba833bcf77398158333f851be30b6
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-10-04 21:50:57 +0300

    gin running

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"example.com/m/v2/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
-	greeting := fmt.Sprintf("Hello")
-	fmt.Println(greeting)
+
+	r := gin.Default()
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.Run()
 }
