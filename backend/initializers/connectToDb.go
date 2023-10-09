be15dacd5f92e75db0f063c7cf8f462f7e924667
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-10-09 21:39:04 +0300

    123

--- backend/initializers/connectToDb.go
+++ backend/initializers/connectToDb.go
@@ -11,10 +11,16 @@ import (
 var DB *gorm.DB
 
 func ConnectToDb() {
-	var err error
-	dsn := os.Getenv("DB")
-	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	//var err error
 
+	postgres_name := os.Getenv("POSTGRES_USER")
+	postgres_user := os.Getenv("POSTGRES_USER")
+	postgres_password := os.Getenv("POSTGRES_PASSWORD")
+	postgres_port := os.Getenv("POSTGRES_PORT")
+
+	result := "host=localhost user=" + postgres_user + " password=" + postgres_password + " dbname=" + postgres_name + " port=" + postgres_port + " sslmode=disable"
+
+	DB, err := gorm.Open(postgres.Open(result), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to db")
 	}
--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"example.com/m/v2/initializers"
-	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -12,11 +11,11 @@ func init() {
 
 func main() {
 
-	r := gin.Default()
+	// r := gin.Default()
 
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "pong"})
-	})
+	// r.GET("/ping", func(ctx *gin.Context) {
+	// 	ctx.JSON(200, gin.H{"message": "pong"})
+	// })
 
-	r.Run()
+	// r.Run()
 }
