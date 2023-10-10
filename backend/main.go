e84d686c43a7e75a6c4238e4ed17599c1ffe5314
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-10-10 21:49:57 +0300

    123

--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"example.com/m/v2/initializers"
-)
-
-func init() {
-	initializers.ConnectToDb()
-	initializers.SyncDatabase()
-}
+import "fmt"
 
 func main() {
-
-	// r := gin.Default()
-
-	// r.GET("/ping", func(ctx *gin.Context) {
-	// 	ctx.JSON(200, gin.H{"message": "pong"})
-	// })
-
-	// r.Run()
+	fmt.Println("Hello, World!")
 }
