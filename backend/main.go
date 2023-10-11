f76ce63d4b4aacf1f6fb99aa617c69b19fa62196
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-10-12 00:28:23 +0300

    123

--- backend/main.go
+++ backend/main.go
@@ -9,10 +9,11 @@ func main() {
 	username := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
 	dbname := os.Getenv("POSTGRES_DB")
+	port := os.Getenv("POSTGRES_PORT")
 
-	connection_data := "postgres://" + username + ":" + password + "@localhost:" +
+	connection_data := "postgres://" + username + ":" + password + "@localhost:" + port + "/" + dbname
 
-		//const conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
+	//const conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
 
-		//fmt.Println(connection_data)
+	fmt.Println(connection_data)
 }
