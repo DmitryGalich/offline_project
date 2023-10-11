e4f775f09d2e4a82937383960dd1afbe4c2ba65b
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-10-12 00:16:29 +0300

    in process

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	username := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
+
+	connection_data := "postgres://" + username + ":" + password + "@localhost:" +
+
+		//const conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
+
+		//fmt.Println(connection_data)
 }
