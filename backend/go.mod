44e6ffced294d3734e21b2bed2c018e17c95af28
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-10-12 23:06:53 +0300

    connection established

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,4 @@ module offline_project
 
 go 1.21.3
 
-//require github.com/lib/pq v1.10.9 // indirect
+require github.com/lib/pq v1.10.9 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	username := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-	port := os.Getenv("POSTGRES_PORT")
+	dbname := "dbname=" + os.Getenv("POSTGRES_DB")
+	username := "user=" + os.Getenv("POSTGRES_USER")
+	password := "password=" + os.Getenv("POSTGRES_PASSWORD")
+	host := "host=offline_project_db.localnet"
+	port := "port=" + os.Getenv("POSTGRES_PORT")
+	sslmode := "sslmode=disable"
 
-	connection_data := "postgres://" + username + ":" + password + "@localhost:" + port + "/" + dbname
-
-	//const conString = "postgres://YourUserName:YourPassword@YourHostname:5432/YourDatabaseName"
+	connection_data := dbname + " " + username + " " + password + " " + host + " " + port + " " + sslmode
 
 	fmt.Println(connection_data)
+
+	db, err := sql.Open("postgres", connection_data)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
