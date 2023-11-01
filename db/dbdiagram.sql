2e534337a7e2e0646d32bbb048ca46bf0b4640cf
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-11-01 20:50:16 +0300

    123

--- db/dbdiagram.sql
+++ db/dbdiagram.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "user" (
-  "id" uuid PRIMARY KEY,
+  "id" uuid PRIMARY KEY uuid_generate_v4(),
   "login" varchar NOT NULL,
   "password" varchar NOT NULL,
   "mail" varchar NOT NULL,
@@ -7,15 +7,15 @@ CREATE TABLE "user" (
   "first_name" varchar NOT NULL,
   "second_name" varchar NOT NULL,
   "date_of_birth" date NOT NULL,
-  "created_at" timestamp NOT NULL
+  "created_at" timestamp NOT NULL now()
 );
 
 CREATE TABLE "chat" (
-  "id" uuid PRIMARY KEY
+  "id" uuid PRIMARY KEY uuid_generate_v4()
 );
 
 CREATE TABLE "chat_comment" (
-  "id" uuid PRIMARY KEY,
+  "id" uuid PRIMARY KEY uuid_generate_v4(),
   "chat" uuid NOT NULL,
   "author" uuid NOT NULL
 );
