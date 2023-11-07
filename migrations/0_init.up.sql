148033bda37859791f133732b8b47d5f8cf96381
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-11-08 01:03:10 +0300

    init migration works

--- migrations/0_init.up.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE "users" (
-  "login" varchar NOT NULL
-);
-
---
+++ migrations/1_init.up.sql
@@ -0,0 +1,28 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE "users" (
+  "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  "login" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "mail" varchar NOT NULL,
+  "phone" varchar NOT NULL,
+  "first_name" varchar NOT NULL,
+  "second_name" varchar NOT NULL,
+  "date_of_birth" date NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE "chats" (
+  "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY
+);
+
+CREATE TABLE "chat_comments" (
+  "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  "chat" UUID DEFAULT uuid_generate_v4() NOT NULL,
+  "author" UUID DEFAULT uuid_generate_v4() NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE "chat_comments" ADD FOREIGN KEY ("chat") REFERENCES "chats" ("id");
+
+ALTER TABLE "chat_comments" ADD FOREIGN KEY ("author") REFERENCES "users" ("id");
