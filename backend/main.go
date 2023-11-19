98a2ad31ad44b552d0c46d808315e23c8e2866d2
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-11-19 19:03:57 +0000

    crud for users

--- backend/main.go
+++ backend/main.go
@@ -28,10 +28,54 @@ func main() {
 
 	offline_project_db := db.New(conn)
 
+	// Creating user
 	user, err := offline_project_db.CreateUser(context.Background(), "LEL")
 	if err != nil {
 		log.Fatal("Cannot CreateUser")
 	}
 
 	log.Println(user)
+
+	{
+		params := db.UpdateUserParams{
+			ID:    user.ID,
+			Login: "KEK",
+		}
+
+		user, err := offline_project_db.UpdateUser(context.Background(), params)
+		if err != nil {
+			log.Fatal("Cannot UpdateUser")
+		}
+
+		log.Println(user)
+	}
+
+	log.Println("USER: ")
+
+	{
+		user, err := offline_project_db.GetUser(context.Background(), user.ID)
+		if err != nil {
+			log.Fatal("Cannot GetUser")
+		}
+
+		log.Println(user)
+	}
+
+	{
+		err := offline_project_db.DeleteUser(context.Background(), user.ID)
+		if err != nil {
+			log.Fatal("Cannot DeleteUser")
+		}
+	}
+
+	log.Println("USERS: ")
+
+	{
+		users, err := offline_project_db.GetUsers(context.Background())
+		if err != nil {
+			log.Fatal("Cannot GetUsers")
+		}
+
+		log.Println(users)
+	}
 }
