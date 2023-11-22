dcab2d185193da9ff1efa5e989df60b6420b7975
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-11-22 16:02:17 +0000

    big user

--- db/query.sql
+++ db/query.sql
@@ -7,9 +7,16 @@ WHERE "id" = $1 LIMIT 1;
 
 -- name: CreateUser :one
 INSERT INTO "users" (
-  "login"
+  "login",
+  "password",
+  "mail",
+  "phone",
+  "first_name",
+  "second_name",
+  "date_of_birth",
+  "created_at"
 ) VALUES (
-  $1
+  $1, $2, $3, $4, $5, $6, $7, $8
 )
 RETURNING *;
 
@@ -17,6 +24,13 @@ RETURNING *;
 UPDATE "users"
 SET
 login = COALESCE($2, login)
+password = COALESCE($3, password)
+mail = COALESCE($4, mail)
+phone = COALESCE($5, phone)
+first_name = COALESCE($6, first_name)
+second_name = COALESCE($7, second_name)
+date_of_birth = COALESCE($7, date_of_birth)
+created_at = COALESCE($7, created_at)
 WHERE "id" = $1
 RETURNING *;
 
--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -2,15 +2,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE "users" (
   "id" UUID NOT NULL DEFAULT (uuid_generate_v4()) PRIMARY KEY,
-  "login" varchar NOT NULL
-  --,
-  --"password" varchar NOT NULL,
-  -- "mail" varchar NOT NULL,
-  -- "phone" varchar NOT NULL,
-  -- "first_name" varchar NOT NULL,
-  -- "second_name" varchar NOT NULL,
-  -- "date_of_birth" date NOT NULL,
-  -- "created_at" timestamp NOT NULL DEFAULT NOW()
+  "login" varchar NOT NULL,
+  "password" varchar NOT NULL,
+  "mail" varchar NOT NULL,
+  "phone" varchar NOT NULL,
+  "first_name" varchar NOT NULL,
+  "second_name" varchar NOT NULL,
+  "date_of_birth" date NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (NOW())
 );
 
 -- CREATE TABLE "chats" (
