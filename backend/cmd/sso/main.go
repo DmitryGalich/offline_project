68cd6adb767f1156acd5d74bb5de70d8255102cf
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-12-04 20:09:31 +0000

    setup logger

--- backend/cmd/sso/main.go
+++ backend/cmd/sso/main.go
@@ -2,11 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"offline_project/internal/config"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-
 	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+	log.Info("Starting application",
+		slog.Any("confg", cfg))
+
+	log.Warn("Warn")
+	log.Error("Error")
+	log.Debug("Debug")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
