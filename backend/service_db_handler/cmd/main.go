b9c8afc2fc3e5df0b7f5809c9de33f197b13c115
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-12-17 19:54:28 +0300

    in process

--- backend/service_db_handler/cmd/main.go
+++ backend/service_db_handler/cmd/main.go
@@ -1,61 +1,109 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+// /*
+//  *
+//  * Copyright 2015 gRPC authors.
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  *     http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  *
+//  */
 
-// Package main implements a server for Greeter service.
+// // Package main implements a server for Greeter service.
+// package main
+
+// import (
+// 	"context"
+// 	"flag"
+// 	"fmt"
+// 	"log"
+// 	"net"
+
+// 	kek "service_db_handler/protobuf/kek/gen/kek"
+
+// 	"google.golang.org/grpc"
+// )
+
+// var (
+// 	port = flag.Int("port", 50051, "The server port")
+// )
+
+// // server is used to implement helloworld.GreeterServer.
+// type server struct {
+// 	kek.UnimplementedGreeterServer
+// }
+
+// // SayHello implements helloworld.GreeterServer
+// func (s *server) SayHello(ctx context.Context, in *kek.HelloRequest) (*kek.HelloReply, error) {
+// 	log.Printf("Received: %v", in.GetName())
+// 	return &kek.HelloReply{Message: "Hello " + in.GetName()}, nil
+// }
+
+// func main() {
+// 	flag.Parse()
+// 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+// 	if err != nil {
+// 		log.Fatalf("failed to listen: %v", err)
+// 	}
+// 	s := grpc.NewServer()
+// 	kek.RegisterGreeterServer(s, &server{})
+// 	log.Printf("server listening at %v", lis.Addr())
+// 	if err := s.Serve(lis); err != nil {
+// 		log.Fatalf("failed to serve: %v", err)
+// 	}
+// }
+
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
-	"log"
 	"net"
-
-	kek "service_db_handler/protobuf/kek/gen/kek"
-
-	"google.golang.org/grpc"
 )
 
-var (
-	port = flag.Int("port", 50051, "The server port")
-)
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 
-// server is used to implement helloworld.GreeterServer.
-type server struct {
-	kek.UnimplementedGreeterServer
-}
+	// Read data from the client
+	buffer := make([]byte, 1024)
+	_, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+		return
+	}
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *kek.HelloRequest) (*kek.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &kek.HelloReply{Message: "Hello " + in.GetName()}, nil
+	// Print received message
+	fmt.Println("Received message:", string(buffer))
+
+	// Respond to the client
+	conn.Write([]byte("Message received!"))
 }
 
 func main() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		fmt.Println("Error listening:", err.Error())
+		return
 	}
-	s := grpc.NewServer()
-	kek.RegisterGreeterServer(s, &server{})
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	defer listener.Close()
+
+	fmt.Println("Server started. Waiting for connections...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting:", err.Error())
+			return
+		}
+		fmt.Println("Client connected:", conn.RemoteAddr())
+
+		// Handle client's connection concurrently
+		go handleConnection(conn)
 	}
 }
--- backend/service_gateway/cmd/main.go
+++ backend/service_gateway/cmd/main.go
@@ -1,43 +1,78 @@
+// package main
+
+// import (
+// 	"context"
+// 	"flag"
+// 	"log"
+// 	kek "service_db_handler/protobuf/kek/gen/kek"
+// 	"time"
+
+// 	"google.golang.org/grpc"
+// 	"google.golang.org/grpc/credentials/insecure"
+// )
+
+// const (
+// 	defaultName = "world"
+// )
+
+// var (
+// 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+// 	name = flag.String("name", defaultName, "Name to greet")
+// )
+
+// func main() {
+// 	flag.Parse()
+
+// 	// Set up a connection to the server.
+// 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+// 	if err != nil {
+// 		log.Fatalf("did not connect: %v", err)
+// 	}
+// 	defer conn.Close()
+
+// 	c := kek.NewGreeterClient(conn)
+
+// 	// Contact the server and print out its response.
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+// 	defer cancel()
+// 	r, err := c.SayHello(ctx, &kek.HelloRequest{Name: *name})
+// 	if err != nil {
+// 		log.Fatalf("could not greet: %v", err)
+// 	}
+// 	log.Printf("Greeting: %s", r.GetMessage())
+// }
+
 package main
 
 import (
-	"context"
-	"flag"
-	"log"
-	kek "service_db_handler/protobuf/kek/gen/kek"
-	"time"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
-
-const (
-	defaultName = "world"
-)
-
-var (
-	addr = flag.String("addr", "localhost:50051", "the address to connect to")
-	name = flag.String("name", defaultName, "Name to greet")
+	"fmt"
+	"net"
 )
 
 func main() {
-	flag.Parse()
-
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		fmt.Println("Error connecting:", err.Error())
+		return
 	}
 	defer conn.Close()
 
-	c := kek.NewGreeterClient(conn)
-
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &kek.HelloRequest{Name: *name})
+	// Send data to the server
+	message := "Hello, server!"
+	_, err = conn.Write([]byte(message))
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		fmt.Println("Error sending:", err.Error())
+		return
 	}
-	log.Printf("Greeting: %s", r.GetMessage())
+
+	// Read response from the server
+	buffer := make([]byte, 1024)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err.Error())
+		return
+	}
+
+	// Print server's response
+	fmt.Println("Server says:", string(buffer))
 }
