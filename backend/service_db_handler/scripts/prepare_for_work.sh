154a8bd6cf6765d6597db2d96c74f663f62bd82d
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-12-17 19:18:58 +0300

    can't ping each other

--- backend/service_db_handler/scripts/prepare_for_work.sh
+++ backend/service_db_handler/scripts/prepare_for_work.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+#go install -v "golang.org/x/tools/gopls@latest" 
 go get "golang.org/x/tools/gopls@latest"
 go install -v "github.com/cweill/gotests/gotests@v1.6.0"
 go install -v "github.com/fatih/gomodifytags@v1.16.0"
 go install -v "github.com/josharian/impl@v1.1.0"
 go install -v "github.com/haya14busa/goplay/cmd/goplay@v1.0.0"
+#go install -v "github.com/go-delve/delve/cmd/dlv@latest"
 go get "github.com/go-delve/delve/cmd/dlv@latest"
+#go install -v "honnef.co/go/tools/cmd/staticcheck@latest"
 go get "honnef.co/go/tools/cmd/staticcheck@latest"
 go install -v "golang.org/x/tools/cmd/goimports@latest"
 
