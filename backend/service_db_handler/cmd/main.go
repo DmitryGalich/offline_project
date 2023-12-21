ab7ac0c560401bdaee4809ee22ca68d1388a8cef
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-12-21 20:03:30 +0300

    removed comments

--- backend/service_db_handler/cmd/main.go
+++ backend/service_db_handler/cmd/main.go
@@ -1,22 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a server for Greeter service.
 package main
 
 import (
@@ -54,51 +35,3 @@ func main() {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"net"
-// )
-
-// func handleConnection(conn net.Conn) {
-// 	defer conn.Close()
-
-// 	// Read data from the client
-// 	buffer := make([]byte, 1024)
-// 	_, err := conn.Read(buffer)
-// 	if err != nil {
-// 		fmt.Println("Error reading:", err.Error())
-// 		return
-// 	}
-
-// 	// Print received message
-// 	fmt.Println("Received message:", string(buffer))
-
-// 	// Respond to the client
-// 	conn.Write([]byte("Message received!"))
-// }
-
-// func main() {
-// 	listener, err := net.Listen("tcp", "192.168.50.20:8080")
-// 	if err != nil {
-// 		fmt.Println("Error listening:", err.Error())
-// 		return
-// 	}
-// 	defer listener.Close()
-
-// 	fmt.Println("Server started. Waiting for connections...")
-
-// 	for {
-// 		conn, err := listener.Accept()
-// 		if err != nil {
-// 			fmt.Println("Error accepting:", err.Error())
-// 			return
-// 		}
-// 		fmt.Println("Client connected:", conn.RemoteAddr())
-
-// 		// Handle client's connection concurrently
-// 		go handleConnection(conn)
-// 	}
-// }
--- backend/service_gateway/cmd/main.go
+++ backend/service_gateway/cmd/main.go
@@ -41,38 +41,3 @@ func main() {
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"net"
-// )
-
-// func main() {
-// 	conn, err := net.Dial("tcp", "192.168.50.20:8080")
-// 	if err != nil {
-// 		fmt.Println("Error connecting:", err.Error())
-// 		return
-// 	}
-// 	defer conn.Close()
-
-// 	// Send data to the server
-// 	message := "Hello, server!"
-// 	_, err = conn.Write([]byte(message))
-// 	if err != nil {
-// 		fmt.Println("Error sending:", err.Error())
-// 		return
-// 	}
-
-// 	// Read response from the server
-// 	buffer := make([]byte, 1024)
-// 	_, err = conn.Read(buffer)
-// 	if err != nil {
-// 		fmt.Println("Error reading response:", err.Error())
-// 		return
-// 	}
-
-// 	// Print server's response
-// 	fmt.Println("Server says:", string(buffer))
-// }
