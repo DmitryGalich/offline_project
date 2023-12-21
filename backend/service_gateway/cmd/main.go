134275a841c81a02381fa232050268649c978131
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2023-12-21 20:02:22 +0300

    docker network script and grpc handshake success

--- backend/service_gateway/cmd/main.go
+++ backend/service_gateway/cmd/main.go
@@ -1,78 +1,78 @@
-// package main
-
-// import (
-// 	"context"
-// 	"flag"
-// 	"log"
-// 	kek "service_db_handler/protobuf/kek/gen/kek"
-// 	"time"
-
-// 	"google.golang.org/grpc"
-// 	"google.golang.org/grpc/credentials/insecure"
-// )
-
-// const (
-// 	defaultName = "world"
-// )
-
-// var (
-// 	addr = flag.String("addr", "localhost:50051", "the address to connect to")
-// 	name = flag.String("name", defaultName, "Name to greet")
-// )
-
-// func main() {
-// 	flag.Parse()
-
-// 	// Set up a connection to the server.
-// 	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-// 	if err != nil {
-// 		log.Fatalf("did not connect: %v", err)
-// 	}
-// 	defer conn.Close()
-
-// 	c := kek.NewGreeterClient(conn)
-
-// 	// Contact the server and print out its response.
-// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-// 	defer cancel()
-// 	r, err := c.SayHello(ctx, &kek.HelloRequest{Name: *name})
-// 	if err != nil {
-// 		log.Fatalf("could not greet: %v", err)
-// 	}
-// 	log.Printf("Greeting: %s", r.GetMessage())
-// }
-
 package main
 
 import (
-	"fmt"
-	"net"
+	"context"
+	"flag"
+	"log"
+	kek "service_db_handler/protobuf/kek/gen/kek"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "192.168.50.20:8080", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "192.168.50.20:8080")
+	flag.Parse()
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		fmt.Println("Error connecting:", err.Error())
-		return
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
-	// Send data to the server
-	message := "Hello, server!"
-	_, err = conn.Write([]byte(message))
-	if err != nil {
-		fmt.Println("Error sending:", err.Error())
-		return
-	}
+	c := kek.NewGreeterClient(conn)
 
-	// Read response from the server
-	buffer := make([]byte, 1024)
-	_, err = conn.Read(buffer)
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &kek.HelloRequest{Name: *name})
 	if err != nil {
-		fmt.Println("Error reading response:", err.Error())
-		return
+		log.Fatalf("could not greet: %v", err)
 	}
-
-	// Print server's response
-	fmt.Println("Server says:", string(buffer))
+	log.Printf("Greeting: %s", r.GetMessage())
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"net"
+// )
+
+// func main() {
+// 	conn, err := net.Dial("tcp", "192.168.50.20:8080")
+// 	if err != nil {
+// 		fmt.Println("Error connecting:", err.Error())
+// 		return
+// 	}
+// 	defer conn.Close()
+
+// 	// Send data to the server
+// 	message := "Hello, server!"
+// 	_, err = conn.Write([]byte(message))
+// 	if err != nil {
+// 		fmt.Println("Error sending:", err.Error())
+// 		return
+// 	}
+
+// 	// Read response from the server
+// 	buffer := make([]byte, 1024)
+// 	_, err = conn.Read(buffer)
+// 	if err != nil {
+// 		fmt.Println("Error reading response:", err.Error())
+// 		return
+// 	}
+
+// 	// Print server's response
+// 	fmt.Println("Server says:", string(buffer))
+// }
