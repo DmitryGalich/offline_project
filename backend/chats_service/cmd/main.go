24847bde20e897ddedc23e79edd4db2323737493
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2024-03-12 19:37:35 +0300

    http dummy

--- backend/chats_service/cmd/main.go
+++ backend/chats_service/cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Hello World")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
