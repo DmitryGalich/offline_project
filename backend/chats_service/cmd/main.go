a94f58897d97930a5e3b675523547903ed622f33
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2024-03-12 22:56:36 +0300

    docker-compose for dev works fine

--- backend/chats_service/cmd/main.go
+++ backend/chats_service/cmd/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Home Page")
+	fmt.Fprintf(w, "Chats service - Home Page")
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Hello World")
 }
 
 func setupRoutes() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/ws", wsEndpoint)
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
 }
 
 func main() {
-    fmt.Println("Starting server")
-    setupRoutes()
-    log.Fatal(http.ListenAndServe(":80", nil))
+	fmt.Println("Starting server")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
