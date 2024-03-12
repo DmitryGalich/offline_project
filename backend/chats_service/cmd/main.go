b8037f55ba71530e56b622de76a8a7e363ed409b
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2024-03-12 21:47:59 +0300

    go works from dockerfile

--- backend/chats_service/cmd/main.go
+++ backend/chats_service/cmd/main.go
@@ -2,25 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
-}
-
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
-}
 
 func main() {
 	fmt.Println("Hello World")
-	setupRoutes()
-	log.Fatal(http.ListenAndServe(":80", nil))
 }
