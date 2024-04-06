75ee18b5e389ebc639a53e1bda4ea29afa84b8cf
dmitry <dmitry@dmitrys-MacBook-Pro.local>
2024-04-06 21:51:49 +0300

    create function and list

--- frontend/lib/components/bottom_navigation_bar.dart
+++ frontend/lib/components/bottom_navigation_bar.dart
@@ -16,39 +16,43 @@ class CustomBottomNavigationBar extends StatefulWidget {
 class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
   int selectedIndex_ = 0;
 
+  final List<BottomNavigationBarItem> icons_ = const [
+    BottomNavigationBarItem(
+      backgroundColor: Colors.black,
+      icon: Icon(
+        Icons.home_filled,
+        size: 35,
+      ),
+      label: '',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(
+        Icons.person_outline,
+        size: 35,
+      ),
+      label: '',
+    ),
+  ];
+
+  void processTap(int index) {
+    setState(() {
+      selectedIndex_ = index;
+      widget.pageController_.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.ease,
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      onTap: (index) {
-        setState(() {
-          selectedIndex_ = index;
-          widget.pageController_.animateToPage(
-            index,
-            duration: const Duration(milliseconds: 100),
-            curve: Curves.ease,
-          );
-        });
-      },
+      onTap: processTap,
       showSelectedLabels: false,
       showUnselectedLabels: false,
       currentIndex: selectedIndex_,
-      items: const [
-        BottomNavigationBarItem(
-          backgroundColor: Colors.black,
-          icon: Icon(
-            Icons.home_filled,
-            size: 35,
-          ),
-          label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.person_outline,
-            size: 35,
-          ),
-          label: '',
-        ),
-      ],
+      items: icons_,
     );
   }
 }
